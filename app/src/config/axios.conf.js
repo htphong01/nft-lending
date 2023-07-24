4056fdb7c136afa819cd66c1af0749695de6d23f
htphong01 <thanhphonga4.hbt@gmail.com>
2023-07-24 14:06:04 +0700

    add production api

--- app/src/config/axios.conf.js
+++ app/src/config/axios.conf.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
-const env = import.meta.env.VITE_ENVIRONMENT
+const env = import.meta.env.VITE_ENVIRONMENT;
 
 const API_URL = {
-    development: 'http://localhost:3000',
-    production: ''
-}
+  development: 'http://localhost:3000',
+  production: 'http://13.215.46.225:3000',
+};
 
 const instance = axios.create({
-    baseURL: API_URL[env],
-    headers: {
-        'Content-Type': 'application/json'
-    }
+  baseURL: API_URL[env],
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default instance;
