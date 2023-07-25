0ff2bc136b3e9f58867132f49b92c811bc7e871e
htphong01 <thanhphonga4.hbt@gmail.com>
2023-07-25 11:25:27 +0700

    update api

--- app/src/config/axios.conf.js
+++ app/src/config/axios.conf.js
@@ -4,7 +4,7 @@ const env = import.meta.env.VITE_ENVIRONMENT;
 
 const API_URL = {
   development: 'http://localhost:3000',
-  production: 'http://13.215.46.225:3000',
+  production: 'http://18.142.250.16:3000',
 };
 
 const instance = axios.create({
