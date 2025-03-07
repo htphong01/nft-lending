df6159308b11992d720c4e9f4b1ac92a8c64e913
titrabui <titrabui@gmail.com>
2025-03-07 17:55:07 +0700

    deploy contracts to testnet

--- blockchain/scripts/deploy.loan.ts
+++ blockchain/scripts/deploy.loan.ts
@@ -1,4 +1,5 @@
 import { ethers } from "hardhat";
+import fs from "fs";
 require("dotenv").config();
 
 async function main() {
@@ -7,9 +8,24 @@ async function main() {
   const deployer = accounts[0];
 
   //* Deploy contracts */
-  const wXENE = "0x1429859428C0aBc9C2C47C8Ee9FBaf82cFA0F20f";
-  const REWARD_PER_BLOCK = ethers.parseEther("1");
-  const START_BLOCK = 0;
+  const MARKETPLACE_FEE_PERCENTAGE = 1000n;
+  const REWARD_PER_BLOCK = ethers.parseEther("0.000001");
+  const START_BLOCK = 931902n;
+
+  console.log("Deploying [ ChonkSociety ] 🚀");
+  const nft = await ethers.deployContract("ChonkSociety", ["test uri"]);
+  await nft.waitForDeployment();
+  console.log(`Deployed Addresses: ${await nft.getAddress()}\n`);
+
+  console.log("Deploying [ WXENE ] 🚀");
+  const wXENE = await ethers.deployContract("WXENE");
+  await wXENE.waitForDeployment();
+  console.log(`Deployed Addresses: ${await wXENE.getAddress()}\n`);
+
+  console.log("Deploying [ Marketplace ] 🚀");
+  const marketplace = await ethers.deployContract("Marketplace", [deployer, deployer, MARKETPLACE_FEE_PERCENTAGE]);
+  await marketplace.waitForDeployment();
+  console.log(`Deployed Addresses: ${await marketplace.getAddress()}\n`);
 
   console.log("Deploying [ LoanChecksAndCalculations ] 🚀");
   const loanChecksAndCalculations = await ethers.deployContract("LoanChecksAndCalculations");
@@ -41,8 +57,27 @@ async function main() {
   await lendingStake.waitForDeployment();
   console.log(`Deployed Addresses: ${await lendingStake.getAddress()}\n`);
 
+  await marketplace.setPaymentToken(wXENE, true);
+
   await lendingPool.setLoan(directLoanFixedOffer);
   await lendingPool.setLendingStake(lendingStake);
+  await lendingPool.setMarketplace(marketplace);
+
+  await directLoanFixedOffer.setNFTPermit(nft, true);
+
+  // Export deployed addresses to JSON
+  const contractAddresses_verify = {
+    LoanChecksAndCalculations: await loanChecksAndCalculations.getAddress(),
+    NFTfiSigningUtils: await nftfiSigningUtils.getAddress(),
+    DirectLoanFixedOffer: await directLoanFixedOffer.getAddress(),
+    LendingPool: await lendingPool.getAddress(),
+    LendingStake: await lendingStake.getAddress(),
+    ChonkSocietyNFT: await nft.getAddress(),
+    WXENE: await nft.getAddress(),
+    Marketplace: await marketplace.getAddress(),
+  };
+  const networkChainID = (await ethers.provider.getNetwork()).chainId;
+  fs.writeFileSync(`deployments/chain-${networkChainID}.json`, JSON.stringify(contractAddresses_verify));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
